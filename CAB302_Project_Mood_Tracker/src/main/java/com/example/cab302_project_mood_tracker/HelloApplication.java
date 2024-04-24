ea966ff7b7991378454eb976344aca895315bad4
awelc <awelch3551@gmail.com>
2024-04-25 00:26:53 +1000

    Basic setup for scenes will sort out more detail later

--- CAB302_Project_Mood_Tracker/src/main/java/com/example/cab302_project_mood_tracker/HelloApplication.java
+++ CAB302_Project_Mood_Tracker/src/main/java/com/example/cab302_project_mood_tracker/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Mood Tracker");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 725);
+        stage.setTitle("Mood Tracker | Login");
         stage.setScene(scene);
         stage.show();
     }
--- CAB302_Project_Mood_Tracker/src/main/java/com/example/cab302_project_mood_tracker/HelloController.java
+++ CAB302_Project_Mood_Tracker/src/main/java/com/example/cab302_project_mood_tracker/HelloController.java
@@ -1,14 +1,39 @@
 package com.example.cab302_project_mood_tracker;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.Console;
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void SwitchToHome(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("home-view.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.setTitle("Mood Tracker | Home");
+
+        scene = new Scene(root, 1280, 725);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void SwitchToLogin(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("login-view.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.setTitle("Mood Tracker | Login");
+
+        scene = new Scene(root, 1280, 725);
+        stage.setScene(scene);
+        stage.show();
     }
 }
