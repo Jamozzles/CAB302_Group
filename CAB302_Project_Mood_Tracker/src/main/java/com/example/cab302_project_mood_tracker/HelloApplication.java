7c1d21718fe16d02f85cf009026cc75f1be18855
awelc <awelch3551@gmail.com>
2024-04-27 21:19:46 +1000

    commit

--- CAB302_Project_Mood_Tracker/src/main/java/com/example/cab302_project_mood_tracker/HelloApplication.java
+++ CAB302_Project_Mood_Tracker/src/main/java/com/example/cab302_project_mood_tracker/HelloApplication.java
@@ -18,8 +18,8 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
         launch();
     }
+
 }
