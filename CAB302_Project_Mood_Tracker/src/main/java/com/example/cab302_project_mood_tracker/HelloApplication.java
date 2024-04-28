ecb0af05c7524697e368e03187a104c782b35978
awelc <awelch3551@gmail.com>
2024-04-28 16:32:59 +1000

    commit
    Signed-off-by: awelc <awelch3551@gmail.com>

--- CAB302_Project_Mood_Tracker/src/main/java/com/example/cab302_project_mood_tracker/HelloApplication.java
+++ CAB302_Project_Mood_Tracker/src/main/java/com/example/cab302_project_mood_tracker/HelloApplication.java
@@ -2,21 +2,31 @@ package com.example.cab302_project_mood_tracker;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
-    private Parent root;
-
+    double x,y =0;
     //Initialises the main scene
     @Override
     public void start(Stage stage) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("login-view.fxml"));
-        SceneLoader.LoadScene(stage,root, "Mood Tracker | Login");
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login-view.fxml")));
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        root.setOnMousePressed(mouseEvent -> {
+            x = mouseEvent.getSceneX();
+            y = mouseEvent.getSceneY();
+        });
+        root.setOnMouseDragged(mouseEvent -> {
+            stage.setX(mouseEvent.getScreenX() - x);
+            stage.setY(mouseEvent.getScreenY() - y);
+        });
+
+        SceneLoader.LoadScene(stage, root, "Mood Tracker | Login");
     }
     public static void main(String[] args) {
         launch();
