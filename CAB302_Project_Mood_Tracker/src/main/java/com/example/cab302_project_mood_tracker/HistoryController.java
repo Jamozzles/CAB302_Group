263a3549bb6fd1350891afb3b3a039866ccd3278
James Martin <james.martin2003@icloud.com>
2024-05-01 20:33:23 +1000

    Added seperate controller for HistoryView

---
+++ CAB302_Project_Mood_Tracker/src/main/java/com/example/cab302_project_mood_tracker/HistoryController.java
@@ -0,0 +1,11 @@
+package com.example.cab302_project_mood_tracker;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.SplitPane;
+
+public class HistoryController {
+
+    @FXML
+    private SplitPane MainSplitPane;
+
+}
